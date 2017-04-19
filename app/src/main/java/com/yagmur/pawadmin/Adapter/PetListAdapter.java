01f772b1b41d2b0fd59453e440d128a75c4aa324
rumayamur <rumayamur@gmail.com>
2017-04-20 00:05:12 +0300

    proje ekleme

---
+++ app/src/main/java/com/yagmur/pawadmin/Adapter/PetListAdapter.java
@@ -0,0 +1,71 @@
+package com.yagmur.pawadmin.Adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.yagmur.pawadmin.Model.Dog;
+import com.yagmur.pawadmin.PetProfileActivity;
+
+import java.util.List;
+
+
+public class PetListAdapter extends RecyclerView.Adapter<PetListAdapter.MyViewHolder> {
+    private List<Dog> dogList;
+
+    static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView petID;
+        Context context;
+        ImageButton likeButton;
+
+        MyViewHolder(View itemView, Context c) {
+            super(itemView);
+            context = c;
+            petID = (TextView) itemView.findViewById(R.id.petId);
+            likeButton = (ImageButton) itemView.findViewById(R.id.petLikeButton);
+            itemView.setOnClickListener(this);
+            likeButton.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            //TODO: like buton görevini ekle!
+            switch (v.getId()) {
+                case R.id.petLikeButton:
+                    Toast.makeText(v.getContext(), "item: " + String.valueOf(getAdapterPosition()), Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    Intent intent = new Intent(context, PetProfileActivity.class);
+                    intent.putExtra("DogId", petID.getText());
+                    context.startActivity(intent);
+            }
+        }
+    }
+
+    public PetListAdapter(List<Dog> dogList) {
+        this.dogList = dogList;
+    }
+
+    @Override
+    public PetListAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.petlist_item, parent, false);
+        return new MyViewHolder(itemView, parent.getContext());
+    }
+
+    @Override
+    public void onBindViewHolder(PetListAdapter.MyViewHolder holder, int position) {
+        Dog dog = dogList.get(position);
+        holder.petID.setText(dog.getDogID());
+    }
+
+    @Override
+    public int getItemCount() {
+        return dogList.size();
+    }
+}
---
+++ app/src/main/java/com/yagmur/pawadmin/PetListActivity.java
@@ -0,0 +1,130 @@
+package com.yagmur.pawadmin;
+
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.yagmur.pawadmin.Adapter.PetListAdapter;
+import com.yagmur.pawadmin.Model.Dog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PetListActivity extends BaseActivity {
+    private Dog dog;
+    private List<Dog> dogList = new ArrayList<>();
+    private RecyclerView petListRV;
+    private PetListAdapter myAdapter;
+    private View alertDialogView;
+    private AlertDialog searchDialog = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Init();
+    }
+
+    private void Init() {
+        petListRV = (RecyclerView) findViewById(R.id.petListRecyclerV);
+        myAdapter = new PetListAdapter(dogList);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        petListRV.setLayoutManager(mLayoutManager);
+        petListRV.setItemAnimator(new DefaultItemAnimator());
+        petListRV.setAdapter(myAdapter);
+
+        prepareDogData();
+
+    }
+
+    public void fabSearch(View v) {
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(PetListActivity.this);
+        alertDialogView = getLayoutInflater().inflate(R.layout.dialog_search, null);
+        alertDialogInit();
+        mBuilder.setView(alertDialogView);
+        searchDialog = mBuilder.create();
+        searchDialog.show();
+
+    }
+
+    private void alertDialogInit() {
+        Spinner speciesSpinner = (Spinner) alertDialogView.findViewById(R.id.species_spinner);
+        ArrayAdapter<CharSequence> speciesAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.species_array, R.layout.spinner_item);
+        speciesAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+        speciesSpinner.setAdapter(speciesAdapter);
+
+        Spinner ageSpinner = (Spinner) alertDialogView.findViewById(R.id.age_spinner);
+        ArrayAdapter<CharSequence> ageAdapter = ArrayAdapter.createFromResource(getApplicationContext(), R.array.age_array, R.layout.spinner_item);
+        ageAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
+        ageSpinner.setAdapter(ageAdapter);
+    }
+
+    public void searchButtonOnClick(View view) {
+        // TODO: 17.4.2017 Search edilen listeyi döndürecek.
+        Snackbar.make(view, "Search OnClick is going to be here", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+    }
+
+    public void cancelButtonOnClick(View view) {
+        searchDialog.dismiss();
+    }
+
+    private void prepareDogData() {
+        dog = new Dog("1000", 2);
+        dogList.add(dog);
+
+        dog = new Dog("1002", 1);
+        dogList.add(dog);
+
+        dog = new Dog("1003", 4);
+        dogList.add(dog);
+
+        dog = new Dog("1004", 6);
+        dogList.add(dog);
+
+        myAdapter.notifyDataSetChanged();
+    }
+
+//    Toolbar menu click kontrol:
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    @Override
+    protected int getMainLayoutID() {
+        return R.layout.petlist_activity_base;
+    }
+
+    @Override
+    protected int getMenuTitleResource() {
+        return R.string.pet_list_title;
+    }
+
+    @Override
+    protected int getMenuLayoutID() {
+        return 0;
+    }
+
+    @Override
+    protected boolean isNavigationDrawerEnabled() {
+        return true;
+    }
+}
